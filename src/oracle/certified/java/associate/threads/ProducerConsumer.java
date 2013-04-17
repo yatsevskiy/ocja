2ff326d17f039e341d2d16623c279c5343b18062
Vladimir Yatsevskiy <v.yatsevskiy@gmail.com>
2013-04-17 17:26:58 +0300

    replace linked list to queue interface. remove comments.

--- src/oracle/certified/java/associate/threads/ProducerConsumer.java
+++ src/oracle/certified/java/associate/threads/ProducerConsumer.java
@@ -2,115 +2,42 @@ package oracle.certified.java.associate.threads;
 
 import java.util.*;
 
-/**
- * Demonstration of the producer/consumer problem in Java.  In this
- * example one producer adds items to a fixed-length queue.  Multiple
- * consumers remove items from the queue.
- *
- * Run this with a smallish buffer (3, perhaps) to verify that the
- * producer waits when the buffer is full (you'll see that the items
- * it adds are never too far ahead of what the consumer is getting)
- * and that the consumer waits when it catches up to the producer.
- *
- * @author Jim Glenn
- * @version 0.2 10/22/2007 (semaphore version)
- * @version 0.1 2/10/2004 (Java synchronization version)
- */
 
 public class ProducerConsumer
 {
-    /**
-     * The fixed-length queue for this demonstration.
-     */
 
     private Buffer b;
     private Semaphore full, empty, mutex;
 
-    /**
-     * Encapsulates the data and operations on the fixed-length queue.
-     */
 
-    public class Buffer
+    public static class Buffer
     {
-    /**
-     * Storage for items on this queue.
-     */
-
-    private LinkedList<Integer> buf;
-
-    /**
-     * The number of items currently on this queue.
-     */
-
-
-    /**
-     * The maximum number of items that can be on this queue.
-     */
-
-
-    /**
-     * Creates a queue that can hold up to the given number of items.
-     *
-     * @param n the capacity of the new queue
-     */
-
+    private Queue<Integer> buf;
     public Buffer(int n)
     {
         buf = new LinkedList<Integer>();
     }
 
-    /**
-     * Removes an item from this queue.  If no item is available,
-     * the calling thread sleeps until one is available.
-     *
-     * @return the item removed
-     */
-
-    public Object remove()
+    public Integer remove()
     {
-        Object result = buf.getFirst();
-        buf.remove(0);
-        return result;
+        return buf.poll();
     }
-
-    /**
-     * Adds an item to this queue.  If there is no more room on the
-     * queue, the calling thread sleeps.
-     *
-     * @param item the item to add
-     */
 
     public void add(Integer item)
     {
-        buf.add(item);
+        buf.offer(item);
     }
     }
 
-    /**
-     * Threads that add items to the queue.
-     */
 
     public class Producer implements Runnable
     {
     private int id;
 
-    /**
-     * The number of items for this thread to add.
-     */
-
-    /**
-     * Creates a producer that will add the given number of items
-     * to the queue.  The items will be numbered 0,...,<CODE>n</CODE>-1
-     */
-
     public Producer(int n)
     {
         id = n;
     }
-
-    /**
-     * Adds items to the queue.
-     */
 
     public void run()
     {
@@ -126,8 +53,6 @@ public class ProducerConsumer
             {
             }
 
-            // code modified from Tanenbaum, p. 112
-
             empty.lock();
             mutex.lock();
             b.add(new Integer(i));
@@ -139,38 +64,22 @@ public class ProducerConsumer
     }
     }
 
-    /**
-     * Threads that remove items from the queue.
-     */
 
-    public class Consumer extends Thread
+    public class Consumer implements Runnable
     {
-    /**
-     * An identifier for this consumer.
-     */
 
     private int id;
-
-    /**
-     * Creates a consumer thread with the given id.
-     *
-     * @param i the id of the new consumer
-     */
 
     public Consumer(int i)
     {
         id = i;
     }
 
-    /**
-     * Causes this consumer to start removing things from the queue.
-     */
 
     public void run()
     {
         while (true)
         {
-            // code modified from Tanenbaum, p. 112
 
             full.lock();
             mutex.lock();
@@ -181,7 +90,7 @@ public class ProducerConsumer
             
             try
             {
-                sleep((int)(Math.random() * 10) * 100);
+                Thread.sleep((int)(Math.random() * 10) * 100);
             }
             catch (InterruptedException e)
             {
@@ -190,16 +99,6 @@ public class ProducerConsumer
     }
     }
 
-    /**
-     * Creates a new example of the producer/consumer problem.
-     * The arguments specify the number of items the producer will add,
-     * the size of the queue that will hold the items, and the number
-     * of consumers that access the queue.
-     *
-     * @param items the number of items for the producer to add
-     * @param bufferSize the size of the fixed-length queue
-     * @param consumers the number of consumers to create
-     */
 
     public ProducerConsumer(int items, int bufferSize, int consumers)
     {
@@ -210,21 +109,13 @@ public class ProducerConsumer
     
     for (int id = 0; id < consumers; id++)
         {
-        Thread t = new Consumer(id);
+        Thread t = new Thread(new Consumer(id));
         t.start();
         }
 
     (new Thread(new Producer(items))).start();
     }
 
-    /**
-     * Creates an instance of the producer/consumer problem given
-     * parameters specified on the command line.
-     *
-     * @param args an array containing the decimal representation of the
-     * number of items to add, the size of the queue, and the number
-     * of consumers in its first three elements
-     */
 
     public static void main(String[] args)
     {
